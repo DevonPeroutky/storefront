906def672ac1efcf51ff020ec8d51a3ba19e4098
Devon Peroutky <devonperoutky@Devons-Mac-mini.local>
2024-02-09 02:05:57 -0800

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Webcam from "react-webcam";
+
+const videoConstraints = {
+  width: 1280,
+  height: 720,
+  facingMode: "user"
+};
+
+const WebcamCapture = () => {
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
+  return (
+    <div>
+      <Webcam
+        audio={false}
+        height={720}
+        screenshotFormat="image/jpeg"
+        width={1280}
+        videoConstraints={videoConstraints}
+      >
+        { /* @ts-ignore */}
+        {({getScreenshot}) => (
+            <button
+                onClick={() => {
+                  const imageSrc = getScreenshot()
+                  console.log("Got screenshot: ", imageSrc);
+                  setImageSrc(imageSrc);
+                }}
+            >
+              Capture photo
+            </button>
+        )}
+      </Webcam>
+      <br/>
+      <ImageDisplay imageSrc={imageSrc}/>
+    </div>
+  )
+};
+
+const ImageDisplay = (props: { imageSrc: string | null }) => {
+  const {imageSrc} = props;
+  if (!imageSrc) {
+    return null;
+  }
+  return (
+      <div>
+        <img src={imageSrc} alt="photo"/>
+      </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="text-3xl font-bold underline">
+            Hello world!
+          </h1>
+          <WebcamCapture/>
+          <br/>
+        </header>
+      </div>
   );
 }
 
